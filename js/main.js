aaa8309d11559b9848cac1548b74e15d1447df79
fajarfadillahh <fajaragustian979@gmail.com>
2022-02-08 10:12:52 +0700

    initial commit & header section

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function showMenu(menuId, toggleId) {
+  const menu = document.getElementById(menuId);
+  const toggle = document.getElementById(toggleId);
+
+  if (menu && toggle) {
+    toggle.addEventListener("click", () => {
+      menu.classList.toggle("show-menu");
+    });
+  }
+}
+showMenu("header-menu", "header-toggle");
