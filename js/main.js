e3d80a056658c1738a945d3fc2747ef829ae0f07
fajarfadillahh <fajaragustian979@gmail.com>
2022-02-12 22:45:44 +0700

    scrollup section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// ===== SHOW HEADER MENU =====
 function showMenu(menuId, toggleId) {
   const menu = document.getElementById(menuId);
   const toggle = document.getElementById(toggleId);
@@ -9,3 +10,15 @@ function showMenu(menuId, toggleId) {
   }
 }
 showMenu("header-menu", "header-toggle");
+
+// ===== SCROLLUP =====
+const scrollY = window.pageYOffset;
+
+function scrollUp() {
+  const scrollup = document.getElementById("scroll-up");
+
+  this.scrollY > 150
+    ? scrollup.classList.add("scroll-action")
+    : scrollup.classList.remove("scroll-action");
+}
+window.addEventListener("scroll", scrollUp);
